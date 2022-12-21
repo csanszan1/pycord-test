beaa64ceee30a93970936d5099de223325f36d67
csanszan1 <csanad@acsanad.eu>
2022-12-21 10:49:38 -0500

    Removed token

--- bot.py
+++ bot.py
@@ -17,4 +17,4 @@ async def on_message(message):
     if message.content.startswith('Hello'):
         await message.channel.send('Hello!')
 
-client.run('MTA1NTE0NzM0NDA2MjA1ODQ5Nw.GfbZxe.-zWKXjnKTN-MvqAhAzWkk56FOl1HNkbTyGs698')
+client.run('TOKEN')
